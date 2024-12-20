51836becafb3851f2b9afd95a44bbeaa58cbeb83
Ivy Tanui <ivy.tanui@strathmore.edu>
2024-12-20 20:32:26 +0300

    Arrays

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Write your solution here!
+// Initial array
+const cats = ["Milo", "Otis", "Garfield"];
+
+// Destructive functions
+function destructivelyAppendCat(name) {
+  cats.push(name); 
+}
+
+function destructivelyPrependCat(name) {
+  cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat() {
+  cats.pop(); 
+}
+
+function destructivelyRemoveFirstCat() {
+  cats.shift(); 
+}
+
+// Nondestructive functions
+function appendCat(name) {
+  return [...cats, name]; 
+}
+
+function prependCat(name) {
+  return [name, ...cats]; 
+}
+
+function removeLastCat() {
+  return cats.slice(0, -1); 
+}
+
+function removeFirstCat() {
+  return cats.slice(1); 
+}
+
